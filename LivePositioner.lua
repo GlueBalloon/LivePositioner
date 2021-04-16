b8543bfba4fd9dc715da712cede202872da8fdb4
Jesse Woner Clark <jwc@jessewonderclark.com>
2021-04-15 21:43:26 -0400

    clear out huge comment blocks in LivePositioner

--- LivePositioner.lua
+++ LivePositioner.lua
@@ -7,15 +7,8 @@ LivePositioner = class()
 -- LivePositioner can be initialized with nothing, with just an entity, with an entity plus intial positioning instructions, or any combination thereof
 -- If not given positionings, it supplies default initial values.
 -- Position instructions must be in the format created by LivePositioner:rangeTable(...)
-function LivePositioner:init(thing, pTable, eTable, sTable)
-    self.subject = thing
-    --[[
-    local position = pTable or self:rangeTable(0.5,0.5,10.5,1000)
-    local eulers = eTable or self:rangeTable(0,0,0,180)
-    local scales = sTable or self:rangeTable(1,1,1,150)
-    self:setNonPositioningParameters()
-    self:define(self.subject, position, eulers, scales)
-    ]]
+function LivePositioner:init(thing)
+    self:changeSubject(thing)
 end
 
 function LivePositioner:changeSubject(thisSubject) 
@@ -35,66 +28,9 @@ function LivePositioner:changeSubject(thisSubject)
     end
     subjectScaleX, subjectScaleY, subjectScaleZ = thisSubject.scale.x, thisSubject.scale.y, thisSubject.scale.z
     self.subjectChanging = false
-    --[[
-    --blooming blooming
-    local material = craft.material(asset.builtin.Materials.Basic)
-    self.subject.material = material
-    material.blendMode = ADDITIVE
-    -- Combine color and intensity for HDR effects
-    local Intensity = 1.5
-    local effectColor = vec3(200/255, 90/255, 30/255) * Intensity
-    self.subject.material.diffuse = effectColor
-    local effectIntensity = vec3(100/255, 90/255, 130/255) * Intensity
-    self.subject.material.diffuse = effectIntensity
-    ]]
 end
 
--- setNonPositioningParameters sets up the watched variables and the button for saving the current settings
---[[
-function LivePositioner:setNonPositioningParameters()
 
-    SaveAndLoadFo = "There is only one save/load slot, so be careful.\n\nWhen you tap 'Save Scene' your current models and positions are stored on a tab named 'recreateScene'.\n\nIf you want to preserve a creation for good, manually copy the contents of that tab somewhere else.\n\nYou can also save just your camera position to a tab called 'restoreCameraSettings'."
-    
-    parameter.boolean("Save And Load Info", true, function(value)
-        if value == false then
-            output.clear()
-            print(SaveAndLoadFo)
-        end
-        Save_And_Load_Info = true
-    end)
-    parameter.watch("Storing")
-    Storing = "Only one save/load slot, so be careful. If you want to keep your current save forever, copy the contents of the 'recreateScene' tab to another location."
-    parameter.action("Save Scene", 
-        function() 
-        EasyCraft.saveScene() 
-        EasyCraft.saveCameraPlacement()
-    end)
-
-    parameter.action("Load Saved Scene",
-    function() 
-
-        entities = {}
-        currentEntityIndex = 1
-        currentSetIndex = 1
-        currentModelIndex = 1
-        
-        local destroyUs = {}
-        for k, v in pairs(EasyCraft.entities) do
-            destroyUs[k] = v
-        end      
-        -- EasyCraft.entities = {}
-
-        local newSceneParts
-        if easyCraftRecreate then
-            newSceneParts = easyCraftRecreate()
-        end
-    end)
-    
-    parameter.action("Save Just Camera Position", function()
-        EasyCraft.saveCameraPlacement()
-    end)
-end
-]]
 
 -- defineLiveParameters(...) sets up the parameter controls
 function LivePositioner:define(thing, pTable, eTable, sTable)
@@ -105,13 +41,6 @@ function LivePositioner:define(thing, pTable, eTable, sTable)
 end
 
 function LivePositioner:setUpParametersWithMicroSettingOf(setting)
-    
-    --[[
-    if not ModelIndexParameterCurrent then
-        ModelIndexParameterCurrent = 1
-        PackIndexParameterCurrent = 1
-    end
-    ]]
     
     parameter.clear()
     
@@ -310,41 +239,7 @@ EasyCraft.entityNames = {}
         print(EasyCraft.entities[entities[currentEntityIndex] ].position)
         self:changeSubject(EasyCraft.entities[entities[currentEntityIndex] ])
     end)    
-    
-    --self.mostRecentMicroSetting = setting
 end
-
-    
---[[
-function LivePositioner:setUpParametersWithMicroSettingOf(setting)
-    
-    PositioningFo = "The positioning sliders let you change the placement, rotation, and size of the selected model.\n\nIf it's hard to get a model in the exact right size, place, or angle that you want, toggle 'MicroMode' on.\n\nMicroMode is for making precise adjustments and getting things juuuuuust right."
-    
-    parameter.boolean("Positioning Info", true, function(value)
-        if value == false then
-            output.clear()
-            print(PositioningFo)
-        end
-        Positioning_Info = true
-    end)
-    
-    parameter.boolean("MicroMode", false, function(microOn)
-        if microOn ~= setting then
-            setUpParametersWithMicroSettingOf(microOn)
-        end
-    end)
-    
-
-    
-    self:setPositionsAndRanges(self:rangeTable(0,0,0,1000))
-    if setting == false then
-        self:setEulersAndRanges(self:rangeTable(0,0,0,180))
-    elseif setting == true then
-        self:setEulersAndRanges(self:rangeTable(subjectEulerX, subjectEulerY, subjectEulerZ,20))
-    end
-    self:setScalesAndRanges(self:rangeTable(1,1,1,150,1,40))
-end
-]]
 
 function LivePositioner:useTablesIn(tableOfPositions)
     subjectX, subjectY, subjectZ = tableOfPositions.position.x, tableOfPositions.position.y, tableOfPositions.position.z
@@ -440,90 +335,5 @@ function LivePositioner:update()
             multiplier = subjectScaleAll
         end
         self.subject.scale = vec3(subjectScaleX * multiplier, subjectScaleY * multiplier, subjectScaleZ * multiplier)
-   --     subjectScale = self.subject.scale
     end
 end
-
-
---[[
--- Macro-Micro Sliders
-
-function setup()
-    scene = craft.scene()
-    scene.camera.position=vec3(0,1,-4)
-    
-    ground = scene:entity()
-    ground.model = craft.model.cube(vec3(1,.2,1))
-    ground.material = craft.material(asset.builtin.Materials.Specular)
-    ground.material.map = readImage(asset.builtin.Surfaces.Desert_Cliff_Roughness)
-    ground.position = vec3(1,1,1)
-    
-    ground2 = scene:entity()
-    ground2.model = craft.model.cube(vec3(1,.2,1))
-    ground2.material = craft.material(asset.builtin.Materials.Specular)
-    ground2.material.map = readImage(asset.builtin.Surfaces.Desert_Cliff_Roughness)
-    ground2.position = vec3(-1,1,1)
-    
-    fill(255)
-    
-    setUpParametersWithMicroSettingOf(false)
-end
-
-function setUpParametersWithMicroSettingOf(setting)
-    if not gx then
-        gx,gy,gz = 0,0,0
-        switchState = false
-    end
-    parameter.clear()
-    parameter.boolean("Micro", setting, function(microOn)
-        if microOn ~= setting then
-            setUpParametersWithMicroSettingOf(microOn)
-        end
-    end)
-    if setting == false then
-        parameter.integer("gx",-180,180,gx)
-        parameter.integer("gy",-180,180,gy)
-        parameter.integer("gz",-180,180,gz)
-    elseif setting == true then
-        parameter.integer("gx",gx-10,gx+10,gx)
-        parameter.integer("gy",gy-10,gy+10,gy)
-        parameter.integer("gz",gz-10,gz+10,gz)
-    end
-    parameter.boolean("switchControlledBlock", switchState, function(bool)
-        if bool == switchState then return end
-        switchState = bool == true
-        local newBlock, oldBlock = ground2, ground
-        if switchState == true then
-            newBlock, oldBlock = ground, ground2
-        end
-        oldBlock.displayEulers = vec3(gx,gy,gz)
-        if newBlock.displayEulers then
-            gx,gy,gz = newBlock.displayEulers.x, newBlock.displayEulers.y, newBlock.displayEulers.z
-        else
-            gx,gy,gz = 0,0,0
-        end
-        if setting == true then
-            setUpParametersWithMicroSettingOf(true)
-        end
-    end)
-end
-
-function update(dt)
-    if switchControlledBlock == true then
-        ground2.rotation=quat.eulerAngles(gx,gy,gz)
-    else
-        ground.rotation=quat.eulerAngles(gx,gy,gz)
-    end
-    scene:update(dt)
-end
-
-function draw()
-    update(DeltaTime)
-    scene:draw()
-    if switchControlledBlock == true then
-        text("eulers control block on right",WIDTH/2,HEIGHT/2+150)
-    else
-        text("eulers control block on left",WIDTH/2,HEIGHT/2-150)
-    end
-end
-]]
