f0ac7f24d7379aad1de0ff31a5f0ad733959d9b2
Jesse Wonder Clark <jwc@jessewonderclark.com>
2021-05-19 15:20:49 -0400

    more work on spider legs

--- restoreCameraSettings.lua
+++ restoreCameraSettings.lua
@@ -9,8 +9,8 @@
         --negative ry rotates you counter-clockwise around the target, so -180 is also completely behind
         --positive rx moves you above the target, with 90 or more being directly overhead
         --negative rx moves you below the target, with -90 or more being directly under
-        viewer.ry = -132.48722830127
-        viewer.rx = 65.72932489375
-        viewer.zoom = 243.48670600806
-        viewer.target = target or vec3(-168.143914, -1.401193, -229.213745)
+        viewer.ry = 155.24462831132
+        viewer.rx = 18.462183964759
+        viewer.zoom = 409.33890074706
+        viewer.target = target or vec3(-78.972543, -40.285579, -3.899990)
 end
